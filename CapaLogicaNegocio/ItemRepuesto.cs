73b8bc8446e579b8eda6f06342605d22de39018d
OmarYagualZambrano <117038185+OmarYagualZambrano@users.noreply.github.com>
2024-11-20 20:49:54 -0500

    Update ItemRepuesto.cs

--- CapaLogicaNegocio/ItemRepuesto.cs
+++ CapaLogicaNegocio/ItemRepuesto.cs
@@ -1,26 +1,21 @@
-﻿using System;
+using System;
 
 namespace ServicioTecnicoCelular.CS
 {
+    // Clase que representa un repuesto como un tipo de ítem
+    // Hereda de la clase base `Item`
     public class ItemRepuesto : Item
     {
+        // Propiedad que almacena el país de importación del repuesto
         public string PaisImportacion { get; set; }
 
-        /*public ItemRepuesto(string nombre, double precio, string categoria_repuesto) : base(nombre, precio)
-        {
-            if (string.IsNullOrWhiteSpace(categoria_repuesto))
-            {
-                throw new ArgumentException("El código no puede estar vacío.", nameof(categoria_repuesto));
-            }
-
-            this.categoria_repuesto = categoria_repuesto;
-        }*/
-        
-
+        // Sobrescribe el método `MostrarInformacion` de la clase base `Item`
         public override void MostrarInformacion()
         {
+            // Llama al método base para mostrar información común del ítem
             base.MostrarInformacion();
-            Console.WriteLine($"Código del Repuesto: {PaisImportacion}");
+            // Muestra información específica del país de importación del repuesto
+            Console.WriteLine($"País de Importación: {PaisImportacion}");
         }
     }
 }
